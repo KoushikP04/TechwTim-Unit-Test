7bb778b21dfce9220b285c4e94907479465b8182
KoushikP04 <paulkoushik04@gmail.com>
2023-03-08 10:08:59 -0500

    First response

--- main.py
+++ main.py
@@ -4,18 +4,19 @@ import page # a file that I created
 
 class PythonOrgSearch(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome(r"C:\Users\paulk\Desktop\Selenium\Selenium101")
+        self.driver = webdriver.Chrome(r"C:\Users\paulk\Desktop\Selenium")
         self.driver.get("http://www.python.org")
+        driver = self.driver
 
     def test_example(self): #if any method starts with "test_"
         print("Test")
         assert True
     
     def not_a_test(self):
-        print("this is true")
+        print("this will not print")
 
     def tearDown(self):
         self.driver.close()
 
-    if __name__ == "__main__":
+if __name__ == "__main__":
         unittest.main()
